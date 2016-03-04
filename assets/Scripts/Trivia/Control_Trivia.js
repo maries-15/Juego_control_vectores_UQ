e074737d591e5b1e93822a06faff252cf70474b4
José Omar Colorado Pérez <jose.tatin@hotmail.com>
2016-03-04 13:49:12 -0500

    Trivia reconstruida
    Se añaden imágenes en vez de texto para cada posible respuesta de la
    trivia, ademas se cambia la forma de evaluar la respuesta.

--- assets/Scripts/Trivia/Control_Trivia.js
+++ assets/Scripts/Trivia/Control_Trivia.js
@@ -2,23 +2,29 @@
 import UnityEngine.UI;
 import System.IO;
 
+public var bien : GameObject;
+public var mal : GameObject;
 public var option_1:UnityEngine.UI.Button;
 public var option_2:UnityEngine.UI.Button;
 public var option_3:UnityEngine.UI.Button;
 public var option_4:UnityEngine.UI.Button;
 public var question:UnityEngine.UI.Text;
+
+
 //Arreglo de todas las preguntas
 private var questions:Array = new Array();
-var conversaciones:String[];
+private var conversaciones:String[];
 
 //Arreglo de la pregunta cargada
 var arrayQue : Array;
 
 public  var respuesta : String = "0";
+private var rUsuario : String = "0"; 
 //Variable para ir llenando la matriz con cada pregunta y conocer el numero de preguntas
 
 
 function Start () {
+
 
 	ChargeQuestions();
 	AleatoryQuestion();
@@ -28,26 +34,33 @@ function Start () {
 function OnClick(_select : String )
 {
 	
-	var _button : Button = pressButton(_select);
-	var t:Text = _button.gameObject.GetComponentInChildren(Text);
+	var _buttonP : Button = pressButton(_select);
+	var t:Text = _buttonP.gameObject.GetComponentInChildren(Text);
 
 
-	if(respuesta.Equals(t.text))
+	if(rUsuario.Equals(respuesta))
 	{
-		print(":D" + _button);
-		_button.GetComponent(Button).colors.highlightedColor = Color.green;
-		//var colorTint : ColorBlock = _button.GetComponent(Button).colors;
-		//colorTint.pressedColor = hexToColor("24EC1DFF");
+		print(":D" + _buttonP);
+		_buttonP.GetComponent(Image).color = Color.green;
+		//bien.enabled = true;
+		bien.SetActive(true);
+		clearButtons();
+
 		//_button.GetComponent("Button").colors.pressedColor = hexToColor("24EC1DFF");
 	}
 	else{
 		print(respuesta+":(");
-		_button.GetComponent(Button).colors.highlightedColor = Color.red;
-
+		_buttonP.GetComponent(Image).color = Color.red;
+		mal.SetActive(true);
+		clearButtons();
 	}
 
 	if(questions.length > 0)
+	{
+		
 		AleatoryQuestion();
+
+	}
 	else
 		SceneManager.LoadScene("cambioImagen");
 }
@@ -59,10 +72,19 @@ function Update () {
 }
 
 function ChangeButtonText(_button : Button, _newText : String) {
+
+
+  
+    var myImage : Sprite = Resources.Load("imagenesTrivia/"+_newText, typeof(Sprite)) as Sprite; // Make sure not to include the file extension
      if (_button!=null)
      {
-     	var t:Text = _button.gameObject.GetComponentInChildren(Text);
-     	t.text = _newText;
+
+     	
+         //Make sure it is added in the Inspector. Or reference it using GameObject.Find.
+        // _button.GetComponent(Button).spriteSwap.targetGraphic = myImage;
+         _button.GetComponent(Image).sprite = myImage;
+        // _button.image.sprite = myImage; // That is right, no need to GetComponent.
+
      }
  }
 
@@ -86,7 +108,7 @@ function ChangeButtonText(_button : Button, _newText : String) {
 
 		for (var r = 0; r < conversaciones.length ; r ++)
 			{
-				print(conversaciones[r]);
+
 				 arrayQue.Push(conversaciones[r]);
 				 if (arrayQue.length ==  6)
 				 {
@@ -97,38 +119,8 @@ function ChangeButtonText(_button : Button, _newText : String) {
 				
 			}
 
-//		try {
-//       
-//        var sr = new StreamReader(path);
-//
-//        line = sr.ReadLine();
-//
-//        while (line != null) {
-//            
-//           	arrayQues.Push(line);
-//
-//            line = sr.ReadLine();
-//            lim ++;
-//            if(lim > 5)
-//            {
-//
-//            	
-//            	questions.Push(arrayQues);
-//            	lim =0;
-//  		  		arrayQues = new Array();
-//
-//
-//            }
-//        }
-//
-//        sr.Close();
-//       
-//    }
-//    catch (e) {
-//        // Let the user know what went wrong.
-//        print("The file could not be read:");
-//        print(e.Message);
-//    }
+			arrayQue = null;
+
  }
 
 	 /*
@@ -137,19 +129,18 @@ function ChangeButtonText(_button : Button, _newText : String) {
 	 function AleatoryQuestion()
 	 {
 
+	 	
 
-	 	if(arrayQue != null)	
-	 		yield WaitForSeconds (2);
+		if (arrayQue != null)
+			yield WaitForSeconds (1);
 
-		clearButtons();
 	 	arrayQue = new Array();
 	 	var sel = Random.Range(0,questions.length);
 	 	arrayQue = questions[sel];
 	 	questions.RemoveAt(sel);
 
 	 	question.text = arrayQue[0];
-	 	var p : int = System.Convert.ToInt32(arrayQue[5]);
-	 	respuesta  = arrayQue[p];
+	 	respuesta  = arrayQue[5];
 
 	 	arrayQue = flushArray(arrayQue);
 
@@ -159,25 +150,45 @@ function ChangeButtonText(_button : Button, _newText : String) {
 	 	ChangeButtonText(option_4, arrayQue[4]);
 
 
-
 	 }
 
 
 	function clearButtons()
 	{
-		option_1.GetComponent(Button).colors.highlightedColor = Color.yellow;
-		option_2.GetComponent(Button).colors.highlightedColor = Color.yellow;
-		option_3.GetComponent(Button).colors.highlightedColor = Color.yellow;
-		option_4.GetComponent(Button).colors.highlightedColor = Color.yellow;
+		yield WaitForSeconds (1);
+		bien.SetActive(false);
+		mal.SetActive(false);
+		option_1.GetComponent(Image).color = Color.white;
+		option_2.GetComponent(Image).color = Color.white;
+		option_3.GetComponent(Image).color = Color.white;
+		option_4.GetComponent(Image).color = Color.white;
+//		option_1.GetComponent(Button).colors.highlightedColor = Color.white;
+//		option_1.GetComponent(Image).color = Color.white;
+//		option_2.GetComponent(Image).color = Color.white;
+//		option_3.GetComponent(Image).color = Color.white;
+//		option_4.GetComponent(Image).color = Color.white;
+
 	}
 
    function pressButton(_sel : String)
    {
-   	 	if(_sel == "1")return option_1;
-   	 	else if (_sel == "2") return option_2;
-   	 	else if (_sel == "3") return option_3;
-   	 	else return option_4;
+   	 	if(_sel == "1"){
+   	 		rUsuario = arrayQue[1];
+   	 		return option_1;
+   	 		}
+   	 	else if (_sel == "2") {
+   	 		rUsuario = arrayQue[2];
+   	 		return option_2;
+   	 	}
+   	 	else if (_sel == "3") {
+   	 		rUsuario = arrayQue[3];
+   	 		return option_3;
+   	 	}
+   	 	else {
 
+ 			rUsuario = arrayQue[4];
+ 			return option_4;
+ 		}
 
    }
 
