35a724ea0a5ad3ca41cca39d741084631b8bac9d
José Omar Colorado Pérez <jose.tatin@hotmail.com>
2016-03-15 23:40:35 -0500

    Sonidos y mas

--- assets/Scripts/Initial Menu/IniciarJuego.js
+++ assets/Scripts/Initial Menu/IniciarJuego.js
@@ -8,18 +8,32 @@ function Start () {
 
 function OnMouseDown()
 {
+
+	cargar();
+
+}
+
+function SalirJuego()
+{
+	
+	Application.Quit();
+}
+
+function cargar()
+{
+
+	yield WaitForSeconds (1f);
+	
 	if(serialization.savedGame.typeInit.Equals("Init")){
 		SceneManager.LoadScene("level"+serialization.savedGame.currentLevel);
+
 	}
 	else if(serialization.savedGame.typeInit.Equals("Menu")){
 		SceneManager.LoadScene("Menu");
 	}
 	else if(serialization.savedGame.typeInit.Equals("CI")){
+		
 		SceneManager.LoadScene("cambioImagen");
 	}
-}
 
-function SalirJuego()
-{
-	Application.Quit();
 }
