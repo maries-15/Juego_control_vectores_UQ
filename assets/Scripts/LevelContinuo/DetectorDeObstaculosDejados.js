953f8a23ad27b30a068aae69be4251938f714fd8
mateo <mateojm94_@gmail.com>
2016-03-15 22:55:05 -0500

    Sonido del juego secuencial Sonido que el personaje emite cuando pierde vida

--- assets/Scripts/LevelContinuo/DetectorDeObstaculosDejados.js
+++ assets/Scripts/LevelContinuo/DetectorDeObstaculosDejados.js
@@ -1,5 +1,8 @@
-﻿function OnTriggerEnter2D(other: Collider2D) {
+﻿var impact : AudioClip; //efecto de sonido cuando se destruye un objeto zancudo
+
+function OnTriggerEnter2D(other: Collider2D) {
 	if(other.tag == 'Enemy'){
+		GetComponent.<AudioSource>().PlayOneShot(impact, 1);
 		controlVidaslc.vidas = controlVidaslc.vidas - 1; 
 	}
 }
