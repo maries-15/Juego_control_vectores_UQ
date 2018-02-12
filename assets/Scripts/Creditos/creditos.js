58c9ef23153959ad58161cc3256952922299ca2f
José Omar Colorado Pérez <jose.tatin@hotmail.com>
2018-02-12 16:19:03 -0500

    Actualización de creditos

--- assets/Scripts/Creditos/creditos.js
+++ assets/Scripts/Creditos/creditos.js
@@ -5,7 +5,7 @@ function Start () {
 }
 
 function Update () {
-	if(this.transform.position.y <= 64){
+	if(this.transform.position.y <= 94){
 		this.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y+velocidad, this.transform.position.z);
 	}else{
 		SceneManager.LoadScene("menuInicial");
